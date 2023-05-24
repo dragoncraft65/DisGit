51d739f31d4e23db5f7fe7ddfad9bcc33f90b3ff
dragoncraft65 <covinomd@gmail.com>
2023-05-23 23:00:56 -0400

    Added in repo manager and started descriptions/READMEs

--- disgit_funcs.py
+++ disgit_funcs.py
@@ -1,5 +1,7 @@
-import os
+# functions that work with the git style implementation
 
+# imports
+import os
 import discord
 
 
@@ -10,21 +12,53 @@ def make_repos_folder():
         os.mkdir("DisGit_Repos")
         print("DisGit Repo folder has been created")
 
+async def create_new_repo(message):
+    if message.content.split()[2] in next(os.walk("DisGit_Repos"))[1]:
+        await message.channel.send("Repo Name already taken.")
+        return
+    else:
+        os.mkdir(f"DisGit_Repos/{message.content.split()[2]}")
+        with open(f"DisGit_Repos/{message.content.split()[2]}/description.txt", "w") as f:
+            f.write("This repo has no description yet")
+        await message.channel.send("Repo successfully created")
+        return
 
+async def remove_repo(message):
+    os.rmdir(f"DisGit_Repos/{message.content.split()[2]}")
+    await message.channel.send("Repo removed")
 def disgit_help_function():
     embed = discord.Embed(title="DisGit Help")
-    embed.add_field(name= "git -h", value = "You got to this menu so you must know how to use it")
+    embed.add_field(name= "git -h", value = "You got to this menu so you must know how to use it", inline=True)
+    embed.add_field(name="git -new", value="Creates a new repository with the passed name", inline=True)
+    embed.add_field(name="git -remove", value="Deletes the repository that is passed", inline=True)
+    embed.add_field(name="git -list", value="Lists all repositories", inline=True)
     return embed
 
+def repo_list():
+    embed = discord.Embed(title="Repository LIst")
+    for folder in next(os.walk("DisGit_Repos"))[1]:
+        with open(f"DisGit_Repos/{folder}/description.txt", "r") as f:
+            description = f.read()
+        embed.add_field(name= folder, value=description)
+    return embed
 async def disgit_message_handler(message, client):
+    channel = message.channel
     if message.author == client.user:
         return
 
     elif message.content.split()[0] == "git":
         if len(message.content.split()) == 1:
-            await message.channel.send("Run git -h to access the help menu if you don't know what your doing")
+            await channel.send("Run git -h to access the help menu if you don't know what your doing")
             return
         elif message.content.split()[1] == "-h":
-            await message.channel.send(embed=disgit_help_function())
+            await channel.send(embed=disgit_help_function())
+        elif message.content.split()[1] == "-new":
+             await create_new_repo(message)
+        elif message.content.split()[1] == "-remove":
+            await remove_repo(message)
+        elif message.content.split()[1] == "-list":
+            await channel.send(embed= repo_list())
+
     else:
         return
+
